e6fe4382e96272e4eaba7a2c96cb553f493e1f80
JodieBrakesThings <niccata24@gmail.com>
2022-10-01 18:26:13 +0300

    Update server-example.py

--- server-example.py
+++ server-example.py
@@ -125,6 +125,6 @@ if __name__ == "__main__":
     # This can be toggled on-demand. Simply set to false to allow connections. Defaults to false.
     #server.rejectClientMode = True
     # Start the server.
-    print(PORT)
+    print(process.env.PORT)
 
 server.run(host='0.0.0.0', port=port)
