ea1fe57c6ff6423fea2711f8b2d2662138b4ecd7
JodieBrakesThings <niccata24@gmail.com>
2022-10-01 18:09:01 +0300

    Update server-example.py

--- server-example.py
+++ server-example.py
@@ -126,5 +126,14 @@ if __name__ == "__main__":
     # This can be toggled on-demand. Simply set to false to allow connections. Defaults to false.
     #server.rejectClientMode = True
     # Start the server.
-    import os from pml import app port = int(os.environ.get('PORT', 5000)) server.run(host='0.0.0.0', port=port)
+    import os
+ON_HEROKU = os.environ.get('ON_HEROKU')
+
+if ON_HEROKU:
+    # get the heroku port
+    port = int(os.environ.get('PORT', 17995))  # as per OP comments default is 17995
+else:
+    port = 3000
+
+server.run(host='0.0.0.0', port=port)
     input("Press enter to exit.")
